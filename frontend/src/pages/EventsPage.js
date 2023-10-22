eb27d3f2d77781fe8b6fd1c6ef44fb97fe9ee1ee
jeongsangtae <wjdtkdxo55@naver.com>
2023-10-23 02:10:30 +0900

    React Complete Training Advanced Routing - challenge 6 finish

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENT = [
+  { id: "e1", title: "Event 1" },
+  { id: "e3", title: "Event 2" },
+  { id: "e3", title: "Event 3" },
+];
+
 const EventsPage = () => {
-  return <h1>Event page!</h1>;
+  return (
+    <>
+      <h1>Event page!</h1>
+      <ul>
+        {DUMMY_EVENT.map((dummyEvent) => (
+          <li>
+            <Link key={dummyEvent.id}>{dummyEvent.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
