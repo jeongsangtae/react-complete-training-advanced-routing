0ee2a029e9aaceabe185425787305bc302612710
jeongsangtae <wjdtkdxo55@naver.com>
2023-10-25 03:50:56 +0900

    React Complete Training Advanced Routing - Extract error data and export responses (EventsPage.js, ErrorPage.js edit & PageContent Component add )

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -16,11 +16,13 @@ const EventsPage = () => {
 export default EventsPage;
 
 export const loader = async () => {
-  const response = await fetch("http://localhost:8080/eventsㄴ");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
-    throw { message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+      status: 500,
+    });
   } else {
     return response;
   }
