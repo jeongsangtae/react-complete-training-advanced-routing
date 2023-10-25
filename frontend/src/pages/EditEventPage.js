56b6a1b80d48cb0eb69dbbbb61025e5d51680683
jeongsangtae <wjdtkdxo55@naver.com>
2023-10-25 17:51:25 +0900

    React Complete Training Advanced Routing - useRouteLoaderData Hook and accessing data from other routes (App.js, EditEventPage.js, EventDetailPage.js ...more edit)

--- frontend/src/pages/EditEventPage.js
+++ frontend/src/pages/EditEventPage.js
@@ -1,5 +1,12 @@
+import { useRouteLoaderData } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 const EditEventPage = () => {
-  return <h1>Edit event page!</h1>;
+  const data = useRouteLoaderData("event-detail");
+  const event = data.event;
+
+  return <EventForm event={event} />;
 };
 
 export default EditEventPage;
